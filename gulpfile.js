348fe05390ce50b213cb077ee31ac8df5895b3dd
vshk <contact@vipintanna.com>
2015-03-18 12:59:56 +0000

    Initial commit for Automated Reponsive inline template

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var buildFolder = './final/'
+        , gulp = require('gulp')
+        , fs = require('fs')
+        inlineCss = require('gulp-inline-css'),
+        cmq = require('gulp-combine-media-queries'),
+        inject = require('gulp-inject'),
+        clean = require('gulp-clean'); 
+
+var filesToMove = [
+        './src/images/**/*.*',
+    ];
+
+// Get version using NodeJs file system
+var getVersion = function () {
+    return fs.readFileSync('Version');
+};
+
+gulp.task('default', ['inject','inline-css-html','move']);
+ 
+
+gulp.task('inline-css-html', function() {
+    return gulp.src('./src/*.html')
+        .pipe(inlineCss()).on('error', errorHandler)
+        .pipe(gulp.dest(buildFolder));
+});
+
+gulp.task('inject', function () {
+  gulp.src('./src/index.html')
+  .pipe(inject(gulp.src(['./src/css/responsive.css']), {
+    starttag: '<!-- inject:css -->',
+    transform: function (filePath, file) {
+      // return file contents as string 
+      return file.contents.toString('utf8')
+    }
+  }))
+  .pipe(gulp.dest(buildFolder));
+});
+
+// gulp.task('cmq', function () {
+//   gulp.src('css/*.css')
+//     .pipe(cmq({
+//       log: true
+//     }))
+//     .pipe(gulp.dest(buildFolder));
+// });
+
+gulp.task('move', function(){
+  // the base option sets the relative root for the set of files,
+  // preserving the folder structure
+  gulp.src(filesToMove, { base: './' })
+  .pipe(gulp.dest(buildFolder));
+});
+
+// Handle the error
+function errorHandler (error) {
+    console.log(error.toString());
+    this.emit('end');
+}
